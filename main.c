c691db638c80ed49c778f6dcf988e3b98ccb87b3
Martin Chudik <martin.chudik98@gmail.com>
2018-11-24 14:28:36 +0100

    main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Projekt 2
+//
+//  Created by Martin Chudik on 24/11/2018.
+//  Copyright © 2018 Martin Chudik. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
