7ef418ac32d7629b169c5b00121a4a9609f558e0
Martin Chudik <martin.chudik98@gmail.com>
2018-11-24 16:03:11 +0100

    funkcia_N

--- main.c
+++ main.c
@@ -17,14 +17,57 @@ typedef struct autopredajca{
     int cena;
     int rok_vyroby;
     char stav_vozidla[200];
+    struct autopredajca *dalsi;
 }AUTOPREDAJCA;
+
+int f_N(AUTOPREDAJCA **n_prv,AUTOPREDAJCA **n_akt,FILE **subor){
+    int pzaznam = 0,i=0;
+    char *pomocna;
+    pomocna = (char*) malloc(sizeof(char));
+    *n_prv = (AUTOPREDAJCA*) malloc(sizeof(AUTOPREDAJCA));
+    *subor = fopen("auta.txt","r");
+    while(!feof(*subor)){
+        fgets(pomocna,200,*subor);
+        if(*pomocna == '$')
+            pzaznam++;
+    }
+    if(*subor == NULL || pzaznam == 0)
+        printf("Zaznamy neboli nacitane\n");
+    else
+        printf("Nacitalo sa %d zaznamov\n",pzaznam);
+    rewind(*subor);
+    fscanf(*subor,"%s",pomocna);
+    fscanf(*subor,"%s",(*n_prv)->kategoria);
+    *n_akt = *n_prv;
+    for(i=0;i<pzaznam;i++){
+        (*n_akt)->dalsi = (AUTOPREDAJCA*) malloc(sizeof(AUTOPREDAJCA));
+        *n_akt = (*n_akt)->dalsi;
+        fscanf(*subor,"%s",(*n_akt)->znacka);
+        *n_akt = (*n_akt)->dalsi;
+        fscanf(*subor,"%s",(*n_akt)->predajca);
+        *n_akt = (*n_akt)->dalsi;
+        fscanf(*subor,"%d",&(*n_akt)->cena);
+        *n_akt = (*n_akt)->dalsi;
+        fscanf(*subor,"%d",&(*n_akt)->rok_vyroby);
+        *n_akt = (*n_akt)->dalsi;
+        fscanf(*subor,"%s",(*n_akt)->stav_vozidla);
+        *n_akt = (*n_akt)->dalsi;
+        fscanf(*subor,"%s",pomocna);
+        *n_akt = (*n_akt)->dalsi;
+        fscanf(*subor,"%s",(*n_akt)->kategoria);
+    }
+    return pzaznam;
+}
 
 int main(){
     char c;
+    int pocetz = 0;
+    AUTOPREDAJCA *m_prv=NULL,*m_akt=NULL;
+    FILE *subor;
     scanf("%c",&c);
     while(1){
         if(c == 'n'){
-            
+            pocetz = f_N(&m_prv,&m_akt,&subor);
         }
         else if(c == 'v'){
             
