b5a1a094962561d0fe3b65f8ea0bb00db0ef146a
Martin Chudik <martin.chudik98@gmail.com>
2018-11-24 14:58:15 +0100

    struktura

--- main.c
+++ main.c
@@ -10,6 +10,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct autopredajca{
+    char kategoria[50];
+    char znacka[50];
+    char predajca[100];
+    int cena;
+    int rok_vyroby;
+    char stav_vozidla[200];
+}AUTOPREDAJCA;
 
 int main(){
     char c;
